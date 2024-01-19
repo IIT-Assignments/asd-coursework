308413169bf53f26837cae5fc8f5eb1a35fef1cb
KavindiGimshani <kavindigimshani999@gmail.com>
2024-01-19 16:37:56 +0530

    created budget item class

--- src/Entity/BudgetItem.java
+++ src/Entity/BudgetItem.java
@@ -1,1 +1,50 @@
-//test
+package Entity;
+
+public class BudgetItem {
+    
+    private int itemID;
+    private double amount;
+    Category category;
+
+    public BudgetItem(){
+
+    }
+    
+    public BudgetItem(int itemID, double amount, Category category) {
+        this.itemID = itemID;
+        this.amount = amount;
+        this.category = category;
+    }
+
+    public int getItemID() {
+        return itemID;
+    }
+
+    public void setItemID(int itemID) {
+        this.itemID = itemID;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public void getBudgetItem(){
+        System.out.println("Item ID : " + itemID);
+        System.out.println("Item Name : " + amount);
+        System.out.println("Category : " + category);
+
+    }
+
+}
