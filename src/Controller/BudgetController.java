b4e0c1ed4bd698c7d28c795ef950127ffdcb9f98
kusal98 <kusalrokes@gmail.com>
2024-01-21 21:57:23 +0530

    add create budget

--- src/Controller/BudgetController.java
+++ src/Controller/BudgetController.java
@@ -2,19 +2,33 @@ package Controller;
 
 import Factory.IAppFactory;
 import Model.Budget;
+import Model.Transaction;
+import View.BudgetView;
+import View.CategoryView;
+import View.TransactionView;
+
 import java.util.ArrayList;
+import java.util.UUID;
 
 public class BudgetController implements IAppFactory {
-    private ArrayList<Budget> budgets = new ArrayList<>();
+    private ArrayList<Budget> budgets;
+    private BudgetView budgetView;
 
+    public BudgetController(ArrayList<Budget> budgets, BudgetView budgetView) {
+        this.budgets = budgets;
+        this.budgetView = budgetView;
+    }
     @Override
     public int generateId() {
-        return 0;
+        UUID uuid = UUID.randomUUID();
+        return Math.abs(uuid.hashCode());
     }
 
     @Override
     public void create() {
-
+        Budget budget = this.budgetView.renderAndCreateBudget();
+        budget.setId(this.generateId());
+        budgets.add(budget);
     }
 
     @Override
--- src/View/BudgetView.java
+++ src/View/BudgetView.java
@@ -1,4 +1,60 @@
 package View;
 
+import Mediator.IControllerMediator;
+import Model.Budget;
+import Model.BudgetItem;
+import Model.Category;
+import Model.Transaction;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class BudgetView {
+
+    private final IControllerMediator mediator;
+
+    public  BudgetView(IControllerMediator mediator) {
+        this.mediator = mediator;
+    }
+    private final Scanner scanner = new Scanner(System.in);
+    public Budget renderAndCreateBudget() {
+        String name;
+        List<BudgetItem> budgetItems = new ArrayList<>();
+        double amount;
+        int categoryId;
+
+        System.out.println("Enter a Budget name: ");
+        name = scanner.nextLine();
+;
+        String isProceed = "yes";
+        while (isProceed.equals("yes")){
+
+            System.out.println("Enter category id: ");
+            categoryId = scanner.nextInt();
+            scanner.nextLine();
+
+            System.out.println("Enter transaction amount: ");
+            amount = scanner.nextDouble();
+            scanner.nextLine();
+
+            Category category = this.mediator.getCategoryById(categoryId);
+            BudgetItem budgetItem = new BudgetItem(amount,category);
+            budgetItems.add(budgetItem);
+
+
+            System.out.print("Do you want to add another item (yes/no) :");
+            isProceed = scanner.nextLine().trim();
+        }
+        if (!name.isEmpty() && !budgetItems.isEmpty())
+        {
+            return new Budget(name, budgetItems);
+        }
+        else
+        {
+            return null;
+        }
+
+
+    }
 }
