60b443b6092f0c1d5da16b0465dee2cc261abfed
kusal98 <kusalrokes@gmail.com>
2024-01-21 21:57:23 +0530

    add create budget

--- src/Factory/AppFactory.java
+++ src/Factory/AppFactory.java
@@ -1,10 +1,13 @@
 package Factory;
 
+import Controller.BudgetController;
 import Controller.CategoryController;
 import Controller.TransactionController;
 import Mediator.ControllerMediator;
+import Model.Budget;
 import Model.Category;
 import Model.Transaction;
+import View.BudgetView;
 import View.CategoryView;
 import View.TransactionView;
 
@@ -13,6 +16,7 @@ import java.util.ArrayList;
 public class AppFactory {
     CategoryController categoryController;
     TransactionController transactionController;
+    BudgetController budgetController;
     public IAppFactory getApp(AppType appType) {
         if (appType == AppType.CATEGORY) {
             if (categoryController != null) {
@@ -23,6 +27,27 @@ public class AppFactory {
                 return new CategoryController(categories, categoryView);
             }
         } else if (appType == AppType.BUDGET) {
+            if (budgetController != null) {
+                return this.budgetController;
+            } else {
+                if (categoryController != null) {
+                    ControllerMediator mediator = new ControllerMediator(this.categoryController);
+                    ArrayList<Budget> budgets = new ArrayList<>();
+                    BudgetView budgetView = new BudgetView(mediator);
+                    this.budgetController = new BudgetController(budgets, budgetView);
+                    return this.budgetController;
+                } else {
+                    ArrayList<Category> categories = new ArrayList<>();
+                    CategoryView categoryView = new CategoryView();
+                    this.categoryController = new CategoryController(categories, categoryView);
+
+                    ControllerMediator mediator = new ControllerMediator(this.categoryController);
+                    ArrayList<Budget> budgets = new ArrayList<>();
+                    BudgetView budgetView = new BudgetView(mediator);
+                    this.budgetController = new BudgetController(budgets, budgetView);
+                    return this.budgetController;
+                }
+            }
 
         } else if (appType == AppType.TRANSACTION) {
             if (transactionController == null) {
@@ -40,6 +65,5 @@ public class AppFactory {
             return null;
         }
 
-        return null;
     }
 }
