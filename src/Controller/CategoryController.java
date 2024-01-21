1519c49ff1f5f48cb397db83fd362428a7dc1bec
senuravihanjayadeva <senurajayadeva@gmail.com>
2024-01-22 00:01:39 +0530

    Implemented delete category

--- src/Controller/CategoryController.java
+++ src/Controller/CategoryController.java
@@ -55,6 +55,12 @@ public class CategoryController implements IAppFactory {
 
     @Override
     public void delete() {
+        int id = categoryView.deleteCategory();
 
+        for(Category category: categories){
+            if(category.getId() == id){
+                categories.remove(category);
+            }
+        }
     }
 }
